a55ce1e1da1eb8e693e750960d8b2fef92b8e7f9
Tiêu Kiệt <tieuminhkiett@gmail.com>
2025-12-29 11:38:07 +0700

    Daily Coding

---
+++ GeeksForGeeks/geeksforgeeks_Find the closest pair from two arrays.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+
+class Solution:
+    def printClosest (self,arr, brr, n, m, x) : 
+        dif = float('inf')
+        N = 0
+        M = m-1
+        
+        dif = float('inf')
+        while N < n and M >= 0:
+            curr_sum = arr[N] + brr[M]
+            
+            if abs(curr_sum - x) < dif:
+                dif = abs(curr_sum - x)
+                res = [arr[N], brr[M]]
+                
+            
+            if curr_sum < x:
+                N += 1
+            elif curr_sum > x:
+                M -= 1
+            else:
+                return res
+        
+        return res
+        
