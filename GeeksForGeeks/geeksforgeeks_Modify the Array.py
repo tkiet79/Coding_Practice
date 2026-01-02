adfd4f99c328cbe670f39d60495f85ef8c11e7a5
Tiêu Kiệt <tieuminhkiett@gmail.com>
2026-01-02 11:21:17 +0700

    Daily Coding

---
+++ GeeksForGeeks/geeksforgeeks_Modify the Array.py
@@ -0,0 +1,20 @@
+#User function Template for python3
+
+class Solution:
+    def modifyAndRearrangeArr (self, arr):
+        idx = 1
+        while idx < len(arr):
+            if arr[idx] == arr[idx-1]:
+                arr[idx-1] = arr[idx-1] * 2
+                arr[idx] = 0
+                idx += 2
+            else:
+                idx += 1
+                continue
+        
+        res = [val for val in arr if val != 0] + [val for val in arr if val == 0]
+        return res
+            
+        
+            
+         
